0c52b03dcaa2a738cd6f9556c6a7cc46763c674d
mtc-li <3280398168@qq.com>
2022-01-30 00:28:55 +0800

    描述

---
+++ src/views/Layout/index.vue
@@ -0,0 +1,20 @@
+<template>
+ <div>
+   wo 是大框架
+  </div> 
+ 
+</template>
+
+<script>
+export default {
+ 
+  
+}
+</script>
+
+<style scoped>
+.main {
+    padding-top: 46px;
+    padding-bottom: 50px;
+}
+</style>
